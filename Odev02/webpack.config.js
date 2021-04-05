bed4d946afbb5ba4fc74e0219860b0969eb22296
yasinblr <80891924+yasinblr@users.noreply.github.com>
2021-04-05 10:05:46 +0300

    Add files via upload

---
+++ Odev02/webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports ={
+
+    entry:'./src/kod.js',
+    output:{
+        filename:"bundle.js",
+        path: path.resolve(__dirname,'public'),
+        libraryTarget:"var",
+        library:"Oyun"
+    },
+
+    devServer: {
+        contentBase:'./public',
+        injectClient:false
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false
+            })
+        ]
+    }
+}
