9155f300c3f74afc0cefba64d3c0879cd7c3c917
yasinblr <80891924+yasinblr@users.noreply.github.com>
2021-04-05 10:09:12 +0300

    Odev02_son

---
+++ odev02/src/kod.js
@@ -0,0 +1,30 @@
+var rand = Math.floor((Math.random()*3));
+
+var lst = [];
+for (var index = 0, len = 3; index < len; ++index) {
+
+   lst[index] = "<img id='"+index+"' onclick='Oyun.tikla(this.id)' class='kart' src='./foto/mavi.jpg'>"
+}
+
+
+const deger = rand;
+var div = document.getElementById('blok');
+div.innerHTML = lst;
+
+var say = 0;
+
+export function tikla(clickId){
+say++;
+
+if(clickId == deger){
+     document.getElementById(clickId).src="./foto/kedi.jpg";
+     document.getElementById("alanId").innerHTML="Kazandın tebrikler! Yeni bir oyun oynamak istersen <a href=''>buraya</a> tıklayabilirsin. ";
+}else{
+  
+    if(say >=2){
+        document.getElementById("alanId").innerHTML="Kaybettin :( Yeni bir oyun oynamak istersen <a href=''>buraya</a> tıklayabilirsin. ";
+
+    }
+    document.getElementById(clickId).src="./foto/kopek.jpg";
+}
+}
